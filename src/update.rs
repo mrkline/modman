d33c07c34b91f92d302e2688f2e6aca4c6094852
Matt Kline <matt@bitbashing.io>
2020-03-21 17:27:34 -0700

    update_file(): Don't take mutable metadata param
    This will help us parallelize it in a minute.

--- src/update.rs
+++ src/update.rs
@@ -1,6 +1,5 @@
-use std::fs::*;
-use std::io::BufReader;
-use std::path::Path;
+use std::fs;
+use std::path::{Path, PathBuf};
 
 use failure::*;
 use log::*;
@@ -61,21 +60,24 @@ fn update_installed_mods(p: &mut Profile, dry_run: bool) -> Fallible<()> {
         if *current_version != *activated_version {
             bail!(
                 "{}'s version ({}) doesn't match what it was when ({}) when it was activated",
-                mod_path.to_string_lossy(),
+                mod_path.display(),
                 current_version,
                 activated_version
             );
         }
 
         for (mod_file_path, metadata) in &mut manifest.files {
-            updates_made |= update_file(
+            if let Some(new_metadata) = update_file(
                 mod_path,
                 mod_file_path,
                 metadata,
                 &*m,
                 &p.root_directory,
                 dry_run,
-            )?;
+            )? {
+                updates_made = true;
+                *metadata = new_metadata;
+            }
         }
         // Ideally we'd like to write out the profile file here,
         // once after each mod we've visited.
@@ -84,7 +86,9 @@ fn update_installed_mods(p: &mut Profile, dry_run: bool) -> Fallible<()> {
     }
 
     if updates_made {
-        update_profile_file(&p)?;
+        if !dry_run {
+            update_profile_file(&p)?;
+        }
     } else {
         info!("Game files haven't changed, no updates needed.");
     }
@@ -112,94 +116,118 @@ fn update_installed_mods(p: &mut Profile, dry_run: bool) -> Fallible<()> {
 fn update_file(
     mod_path: &Path,
     mod_file_path: &Path,
-    metadata: &mut ModFileMetadata,
+    old_metadata: &ModFileMetadata,
     m: &dyn Mod,
     root_directory: &Path,
     dry_run: bool,
-) -> Fallible<bool> {
-    let mut updated_file = false;
-
+) -> Fallible<Option<ModFileMetadata>> {
     let game_path = mod_path_to_game_path(mod_file_path, root_directory);
     let game_hash = hash_file(&game_path)?;
-    if game_hash != metadata.mod_hash {
-        trace!(
-            "{} hashed to\n{:x},\nexpected {:x}",
-            game_path.to_string_lossy(),
-            game_hash.bytes,
-            metadata.mod_hash.bytes
-        );
-        updated_file = true;
-        if dry_run {
-            let hash = hash_file(&game_path)?;
-            trace!(
-                "Game file {} hashed to\n{:x}",
-                game_path.to_string_lossy(),
-                hash.bytes
-            );
-            println!(
-                "{} was changed and needs its backup updated",
-                mod_file_path.to_string_lossy()
-            );
-        } else {
-            let game_file_path = mod_path_to_game_path(mod_file_path, root_directory);
-
-            info!(
-                "{} changed. Backing up new version and reinstalling mod file.",
-                game_file_path.to_string_lossy()
-            );
-
-            let game_hash = hash_and_backup_file(mod_file_path, root_directory)?;
-
-            // This is very simimlar to what `modman activate` is doing
-            // to initially install mods, but it has a few differences
-            // (we don't have to worry about a dry run hashing the mod file again,
-            // we don't have to create directories, etc.)
-            // But should we factor them into a common function to their traces
-            // and behavior in sync anyways?
-            let mut mod_file_reader = m.read_file(&mod_file_path)?;
-            let mut game_file = File::create(&game_file_path).with_context(|_| {
-                format!("Couldn't overwrite {}", game_file_path.to_string_lossy())
-            })?;
-
-            let mod_hash = hash_and_write(&mut mod_file_reader, &mut game_file)?;
-
-            let full_mod_path: String = mod_path.join(mod_file_path).to_string_lossy().into_owned();
-            trace!("Mod file {} hashed to\n{:x}", full_mod_path, mod_hash.bytes);
-
-            // TODO Update metadata and write it out
-            if metadata.mod_hash != mod_hash {
-                warn!(
-                    "The mod file {} doesn't hash to what it did last time it was installed!",
-                    full_mod_path
-                );
-                metadata.mod_hash = mod_hash;
-            }
-            metadata.original_hash = Some(game_hash);
-        }
+    if game_hash == old_metadata.mod_hash {
+        // Cool, nothing changed
+        return Ok(None);
     }
-    Ok(updated_file)
+
+    trace!(
+        "{} hashed to\n{:x},\nexpected {:x}",
+        game_path.display(),
+        game_hash.bytes,
+        old_metadata.mod_hash.bytes
+    );
+
+    if dry_run {
+        println!(
+            "{} was changed and needs its backup updated",
+            mod_file_path.display()
+        );
+        return Ok(Some(ModFileMetadata {
+            mod_hash: old_metadata.mod_hash.clone(),
+            original_hash: Some(game_hash),
+        }));
+    }
+
+    info!(
+        "{} changed. Backing up new version and reinstalling mod file.",
+        game_path.display()
+    );
+
+    backup_file(&game_path, mod_file_path)?;
+
+    // This is very simimlar to what `modman activate` is doing
+    // to initially install mods, but it has a few differences
+    // (we don't have to worry about a dry run hashing the mod file again,
+    // we don't have to create directories, etc.)
+    // But should we factor them into a common function to their traces
+    // and behavior in sync anyways?
+    let mut mod_file_reader = m.read_file(&mod_file_path)?;
+    let mut game_file = fs::File::create(&game_path)
+        .with_context(|_| format!("Couldn't overwrite {}", game_path.display()))?;
+
+    let mod_hash = hash_and_write(&mut mod_file_reader, &mut game_file)?;
+
+    let full_mod_path = mod_path.join(mod_file_path);
+    trace!(
+        "Mod file {} hashed to\n{:x}",
+        full_mod_path.display(),
+        mod_hash.bytes
+    );
+
+    let new_metadata = ModFileMetadata {
+        mod_hash,
+        original_hash: Some(game_hash),
+    };
+
+    // TODO Update metadata and write it out
+    if old_metadata.mod_hash != new_metadata.mod_hash {
+        warn!(
+            "The mod file {} doesn't hash to what it did last time it was installed!",
+            full_mod_path.display()
+        );
+    }
+
+    Ok(Some(new_metadata))
 }
 
 /// Given a mod path, hash and backup the corresponding game file.
 /// Like try_hash_and_backup() from `modman activate`, but doesn't have to deal
 /// with the possibility that the game file isn't there.
-fn hash_and_backup_file(mod_file_path: &Path, root_directory: &Path) -> Fallible<FileHash> {
-    let game_file_path = mod_path_to_game_path(mod_file_path, root_directory);
+fn backup_file(game_file_path: &Path, mod_file_path: &Path) -> Fallible<()> {
+    debug!("Backing up {}", game_file_path.display());
 
-    let game_file = File::open(&game_file_path)
-        .with_context(|_| format!("Couldn't open {}", game_file_path.to_string_lossy()))?;
+    // First, copy the file to a temporary location, hashing it as we go.
+    let temp_file_path = mod_path_to_temp_path(mod_file_path);
+    trace!(
+        "Copying {} to {}",
+        game_file_path.display(),
+        temp_file_path.display()
+    );
+    fs::copy(game_file_path, &temp_file_path)?;
 
-    let hash = hash_and_backup(
-        mod_file_path,
-        &game_file_path,
-        &mut BufReader::new(game_file),
-        BackupBehavior::ReplaceExisting,
-    )?;
+    // Next, create any needed directory structure.
+    let mut backup_file_dir = PathBuf::from(BACKUP_PATH);
+    if let Some(parent) = mod_file_path.parent() {
+        backup_file_dir.push(parent);
+    }
+    fs::create_dir_all(&backup_file_dir)
+        .with_context(|_| format!("Couldn't create directory {}", backup_file_dir.display()))?;
+
+    let backup_path = backup_file_dir.join(mod_file_path.file_name().unwrap());
+    debug_assert!(backup_path == mod_path_to_backup_path(mod_file_path));
 
     trace!(
-        "Game file {} hashed to\n{:x}",
-        game_file_path.to_string_lossy(),
-        hash.bytes
+        "Renaming {} to {}",
+        temp_file_path.display(),
+        backup_path.display(),
     );
-    Ok(hash)
+
+    // Move the backup from the temporary location to its final spot
+    // in the backup directory.
+    fs::rename(&temp_file_path, &backup_path).with_context(|_| {
+        format!(
+            "Couldn't rename {} to {}",
+            temp_file_path.display(),
+            backup_path.display()
+        )
+    })?;
+    Ok(())
 }
